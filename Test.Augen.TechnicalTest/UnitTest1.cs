5d8b611b820b55cca21a82bda7d04a6c272693dd
Binh <ntbinh100488@gmail.com>
2019-11-11 08:48:40 +0700

    unit test

---
+++ Test.Augen.TechnicalTest/UnitTest1.cs
@@ -0,0 +1,25 @@
+using NUnit.Framework;
+
+namespace Test.Augen.TechnicalTest
+{
+    public class Tests
+    {
+
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+
+        [Test]
+        public void Test2()
+        {
+            Assert.Pass();
+        }
+    }
+}
